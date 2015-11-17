d5380df261f72275442c96a005bf624eb3a032e8
Stephen Florian <steezeven@gmail.com>
2015-11-17 13:47:00 -0700

    added routes

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.post('/new-poll', function(req ,res){
       option_4: req.body.option_4,
       option_5: req.body.option_5
     })
-    res.send({
+    res.json({
       poll_id: poll.id, 
       option_1: req.body.option_1,
       option_2: req.body.option_2,
@@ -37,8 +37,32 @@ router.post('/vote/:poll_id', function(req, res){
   })
 })
 
-router.get('*', function(req, res, next) {
-  res.render('index');
-});
+router.get('/poll/:id/results', function(req, res){
+  Promise.all([
+    db.countVotes(req.params.id),
+    db.getVoters(req.params.id)
+  ]).then(function(results){
+    var currentData = {
+      results: results[0],
+      publicVotes: results[1]
+    }
+    currentData.id = req.params.id;
+    currentData.topic = req.body.topic; 
+    currentData.creator = req.body.creator;
+    res.json(currentData).status(200).end()
+  })
+})
+
+
+
+router.post('/:id/vote', function(req, res){
+  db.insert('votes', {
+    poll_id: req.params.id, 
+    name: req.body.name,
+    vote: req.body.vote
+  }).then(function(_){
+    res.status(200).end()
+  })
+})
 
 module.exports = router;
