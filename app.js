c02f20970ff6d56bc1b348e19693d67d706d3232
Stephen Florian <steezeven@gmail.com>
2015-11-20 10:36:16 -0700

    added self domain to allowed cors

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({allowedOrigins: [
-        '*'
-    ]}));
+    'sheltered-basin-3256.herokuapp.com/'
+  ]}));
 
 app.use('/', routes);
 
